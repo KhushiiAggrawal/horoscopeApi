764dfa77d68f2a726230c84253eccbdab9c329a2
khushi kandoi <khushikandoii09@gmail.com>
2023-10-22 01:07:47 +0530

    added changes

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
 import horoscopeRouter from "./routes/horoscope.js";
 
 export const app = express();
 app.use(express.json());
-app.get("/horoscope",horoscopeRouter)
+//CORS
+app.use(
+  cors({
+    origin: ["*", "http://localhost:5000", "http://localhost:3000"],
+    method: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+    credentials: true,
+  })
+);
+app.use("/horoscope", horoscopeRouter);
 app.get("/", (req, res) => {
-    res.status(200).json({ success: true, message: "All Systems Operational" });
-  });
-  
-
+  res.status(200).json({ success: true, message: "All Systems Operational" });
+});
--- models/horoscope.js
+++ models/horoscope.js
@@ -11,20 +11,4 @@ const horoscopeSchema = new mongoose.Schema({
     required: true,
   }
 });
-
-//added virtuals
-const virtual = horoscopeSchema.virtual("id");
-virtual.get(function () {
-  return this._id;
-});
-
-horoscopeSchema.set("toJSON", {
-  virtuals: true,
-  versionKey: false,
-  transform: (doc, ret) => {
-    delete ret._id;
-    delete ret.salt;
-  },
-});
-
 export const horoscopeModel = mongoose.model("horosope", horoscopeSchema);
